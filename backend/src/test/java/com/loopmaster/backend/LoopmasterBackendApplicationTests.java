6dab4ef052720066bbf39d9a3d7762269317e711
Ignacio Ampurdanes <ignaciopercusion@gmail.com>
2026-04-15 12:10:57 +0200

    backend: configuración Spring Boot con MySQL

---
+++ backend/src/test/java/com/loopmaster/backend/LoopmasterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loopmaster.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopmasterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
